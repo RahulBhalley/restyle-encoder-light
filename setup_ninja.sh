5b6772bcc251a8572dceae5d0c79c21bdfb8cf23
Rahul Bhalley <0behinds_secant@icloud.com>
2022-04-23 10:54:35 +0530

    Added `setup_ninja.sh`.

---
+++ setup_ninja.sh
@@ -0,0 +1,3 @@
+wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+sudo unzip ninja-linux.zip -d /usr/local/bin/
+sudo update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force
